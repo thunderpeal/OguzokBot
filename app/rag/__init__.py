eb3339171a9d8637ec66a0a5d82a4fccfc307230
thunderpeak <pealsgun@gmail.com>
2025-10-03 16:13:39 +0300

    index update

--- app/rag/__init__.py
+++ app/rag/__init__.py
@@ -1,8 +1,9 @@
-from .rag import QuoteStore, ensure_index
+from .rag import QuoteStore, ensure_index, build_index
 from .tagger import generate_tags_for_post
 
 __all__ = [
     "QuoteStore",
+    "build_index",
     "ensure_index",
     "generate_tags_for_post",
 ]
